a2fe9c0bfe35fa24bccc7413975967f59ce9baa7
Victor H <vhisc.valo@gmail.com>
2018-09-09 15:34:47 -0500

    Agregar archivos de proyecto.

---
+++ Vtkn_Nexo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Vtkn_Nexo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
